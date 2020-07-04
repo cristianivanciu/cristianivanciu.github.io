112f68cd6cf7f87e1fd08dd78537c3a4b6dbeb54
cristianivanciu <59887988+cristianivanciu@users.noreply.github.com>
2020-07-04 16:54:16 +0300

    Add files via upload

---
+++ PASS/password.js
@@ -0,0 +1,60 @@
+// construim o noua aplicatie vue
+var app = new Vue({
+    el: '#app',
+    data: {
+        password: '',
+        score: 0,
+        message: 'very week', 
+        messages: [
+            'very week',
+            'very week',
+            'week',
+            'medium',
+            'strong',
+            'very strong'
+        ]
+    },
+    methods: {
+        evaluateScore: function() {
+            // definim noul score initial egal cu 0
+            let newScore = 0
+            // evaluam fiecare conditie dintre cele 5
+            // adaugam 1 la noul score pentru fiecare conditie adevarata
+            newScore += this.hasSmallLetter();
+            newScore += this.hasUpperLetter();
+            newScore += this.hasDigit();
+            newScore += this.hasSpecialChar();
+            newScore += this.isLongEnough();
+
+            // alt exemplu la ce este mai sus
+            newScore = [
+                this.hasSmallLetter,
+                this.hasUpperLetter,
+                this.hasDigit,
+                this.hasSpecialChar,
+                this.isLongEnough
+            ]. reduce((score, func) => score + func(), 0);
+        
+            // inlocuim vechiul score cu noul score
+            this.score = newScore;
+            // inlocuim mesajul cu noul mesaj asociat noului scor
+            this.message = this.messages[newScore];
+        },
+        hasSmallLetter: function() {
+            return /[a-z]/.test(this.password);
+        },
+        hasUpperLetter: function() {
+            return /[A-Z]/.test(this.password);
+        },
+        hasDigit: function() {
+            return /[0-9]/.test(this.password);
+        },
+        hasSpecialChar: function() {
+            return /[^a-zA-Z0-9]/.test(this.password);
+        },
+        isLongEnough: function() {
+            return this.password.length >= 8;
+        }
+
+    }
+});
